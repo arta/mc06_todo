675e8f04843b54ed36da431bfdf75ba1895e6659
Martin Fencl <martin.fencl@gmail.com>
2017-01-06 20:51:25 -0600

    Can mark todo_item as completed_at.

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
 
   # POST /todo_lists/:todo_list_id/todo_items
   # =form_for [@todo_list, @todo_item] ..
@@ -11,7 +12,6 @@ class TodoItemsController < ApplicationController
   # DELETE /todo_lists/:todo_list_id/todo_items/:id
   # =link_to .. [@todo_list, @todo_item], method: :delete ..
   def destroy
-    @todo_item = TodoItem.find( params[:id] )
     if @todo_item.destroy
       flash[:success] = 'TODO item successfully deleted.'
     else
@@ -20,10 +20,22 @@ class TodoItemsController < ApplicationController
     redirect_to @todo_list
   end
 
+  # PATCH /todo_lists/:todo_list_id/todo_items/:id/complete
+  # =link_to .. [@todo_list, @todo_item], method: :patch ..
+  def complete
+    @todo_item.update_attribute :completed_at, Time.now
+    redirect_to @todo_list, notice:'TODO item successfully completed.'
+  end
+
   private
     def set_todo_list
       @todo_list = TodoList.find( params[:todo_list_id] )
     end
+
+    def set_todo_item
+      @todo_item = TodoItem.find( params[:id] )
+    end
+
     def todo_item_params
       params.require( :todo_item ).permit( :content )
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   resources :todo_lists do
-    resources :todo_items, only: [:create, :destroy]
+    resources :todo_items, only: [:create, :destroy] do
+      patch 'complete', on: :member
+    end
   end
 
   root 'todo_lists#index'
