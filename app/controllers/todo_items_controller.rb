d9a6bf96bfaf480fc37db6c9e0961d6df6444e2b
Martin Fencl <martin.fencl@gmail.com>
2017-01-06 19:28:35 -0600

    Delete todo_item.

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,14 +1,25 @@
 class TodoItemsController < ApplicationController
+  before_action :set_todo_list
 
   # POST /todo_lists/:todo_list_id/todo_items
   # =form_for [@todo_list, @todo_item] ..
   def create
-    @todo_list = TodoList.find( params[:todo_list_id] )
     @todo_item = @todo_list.todo_items.create( todo_item_params )
     redirect_to @todo_list, notice: 'TODO item successfully added'
   end
 
+  # DELETE /todo_lists/:todo_list_id/todo_items/:id
+  # =link_to .. [@todo_list, @todo_item], method: :delete ..
+  def destroy
+    @todo_item = TodoItem.find( params[:id] )
+    @todo_item.destroy
+    redirect_to @todo_list, notice:'TODO item successfully deleted.'
+  end
+
   private
+    def set_todo_list
+      @todo_list = TodoList.find( params[:todo_list_id] )
+    end
     def todo_item_params
       params.require( :todo_item ).permit( :content )
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :todo_lists do
-    resources :todo_items, only: :create
+    resources :todo_items, only: [:create, :destroy]
   end
 
   root 'todo_lists#index'
